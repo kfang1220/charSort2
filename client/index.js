1a78edbfc9c6a23db4cf9f4369fab64b121399c2
kevinfang <fangk1220@gmail.com>
2018-01-25 13:01:10 -0800

    functioning but not ideal

---
+++ client/index.js
@@ -0,0 +1,20 @@
+//select our form and give it an action
+document.querySelector('#charSort').addEventListener('sort', event => {
+   event.preventDefault();
+
+   let userInput = document.querySelector('input[name=str]');
+   let userResponse = document.querySelector('input[name=result]')
+
+   //should use axios but trying fetch, directly off develop.moz for reference
+
+   fetch('/', {
+     method: 'POST',
+     body: JSON.stringify({input: userInput}),
+     headers: new Headers({
+      'Content-Type': 'application/json'
+     })
+   })
+   .then(res => res.json())
+   .catch(error => console.error('Error:', error))
+   .then(res => {userResponse.value = res.body)};
+})
--- server/index.js
+++ server/index.js
@@ -10,6 +10,16 @@ app.get('/', (req,res) => {
   console.log('hello');
 })
 
+app.post('/', (req,res) => {
+  res.send(sort(req.body.input))
+})
+
+//functions, i could create another file but a small function alone in a file seems pointless
+//function breaks the string sorts and joins back to return
+const sort = (string) => {
+  return string.split('').sort().join('')
+}
+
 app.listen(3000, ()=> {
   console.log('Listening on port 3000!')
 })
